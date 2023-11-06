5345a8f0ec93ddb4bfae20863bc820dccb5f6009
Chad Buntrakulsuk <chad.buntrakulsuk@gmail.com>
2023-11-06 11:54:53 -0800

    added modal for mobile screens

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,10 +11,10 @@ const Modal = ({ children, onClose }: Props) => {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="bg-black opacity-40 w-full h-full absolute sm:bg-white sm:opacity-100"></div>
       <div className="z-50 h-[415px] w-[400px] rounded-lg bg-white absolute top-44">
-        <div className="m-7 text-darkText text-4xl font-bold tracking-normal flex items-center justify-center">
+        <div className="m-7 text-darkText text-4xl font-bold tracking-normal flex sm:justify-center">
           {children}
           <button
-            className="flex items-center flex-grow flex-row-reverse"
+            className="flex items-center flex-grow flex-row-reverse sm:hidden"
             onClick={onClose}
           >
             <svg
@@ -37,6 +37,25 @@ const Modal = ({ children, onClose }: Props) => {
           <img className="py-5" src={ImageRules} />
         </div>
       </div>
+      <button
+        className="hidden sm:absolute sm:bottom-40 sm:block"
+        onClick={onClose}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="black"
+          className="w-6 h-6 scale-150 opacity-20"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
